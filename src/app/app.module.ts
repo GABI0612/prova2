a540d6d94a990dd611b35745e9054f97cd15ec28
gabriel sena <mgms.lic20@uea.edu.br>
2023-07-07 19:48:33 -0300

    version 0.0.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { AddItemComponent } from './add-item/add-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    AddItemComponent
   ],
   imports: [
     BrowserModule,
